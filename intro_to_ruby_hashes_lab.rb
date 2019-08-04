61f0eb03a85396a0d9cbe0d578e467ebb04e6cb3
Chaya Deaver <chayadeaver@gmail.com>
2019-08-04 16:03:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,14 @@
 def base_hash
   monopoly = {
-    :railroads=> {{{}}}
+    :railroads => {
+      {
+        {
+          
+        }
+        
+      }
+      
+    }
   }
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
