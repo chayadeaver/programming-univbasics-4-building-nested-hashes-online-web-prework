05ade5b0fadbb99afe47601886beaedf20b5ca20
Chaya Deaver <chayadeaver@gmail.com>
2019-08-04 16:17:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,8 @@ def monopoly_with_third_tier
           },
         :names = {
           :reading_railroad
-          :penn
+          :pennsylvania_railroad
+          
         }
       }
     }
