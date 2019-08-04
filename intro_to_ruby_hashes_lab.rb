4be347fd87f5d8d96726c1b1a921a9832f9c6241
Chaya Deaver <chayadeaver@gmail.com>
2019-08-04 16:10:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,13 @@ def monopoly_with_third_tier
   monopoly = {
     :railroads => {
       {
-        :pieces => 4
-        :rent_in_dollars 
+        :pieces => 4,
+        :rent_in_dollars = {
+          :one_piece_owned
+          :two_piece_owned
+          :three_piece_owned
+          :four_piece_owned
+        }
       }
     }
   }
